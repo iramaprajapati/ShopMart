2b38d8ecff2956ae9f54ade7d143fa0146959568
Ramprasad Prajapati <ramaprajapati95@gmail.com>
2023-03-24 23:04:57 +0530

    Day 4 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_mart/pages/home_page.dart';
 import 'package:shop_mart/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(ShopMart());
@@ -13,9 +14,12 @@ class ShopMart extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/HomePage",
+      initialRoute: "/LoginPage",
       routes: {
         "/": (context) => LoginPage(),
         "/HomePage": (context) => HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,12 +6,46 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/loginImg.png", fit: BoxFit.cover),
+          // ignore: prefer_const_constructors
+          SizedBox(height: 20),
+          // ignore: prefer_const_constructors
+          Text(
+            "Welcome",
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(height: 20),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    hintText: "Enter Username",
+                  ),
+                ),
+                TextFormField(
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter Password",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
